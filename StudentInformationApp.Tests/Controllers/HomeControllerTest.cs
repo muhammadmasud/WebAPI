c4a5c3866a83c60f3ca433c0f0303d14fd7c68a3
muhammadmasud <jamdmasud@yahoo.com>
2016-10-06 01:22:51 +0600

    done

---
+++ StudentInformationApp.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudentInformationApp;
+using StudentInformationApp.Controllers;
+
+namespace StudentInformationApp.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
