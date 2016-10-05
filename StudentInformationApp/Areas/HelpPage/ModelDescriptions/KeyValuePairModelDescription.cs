531b99a5dec65a20dc0532a59853156c08ade10c
muhammadmasud <jamdmasud@yahoo.com>
2016-10-06 01:22:15 +0600

    ok

--- StudentInformationApp/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace StudentInformationApp.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
--- StudentInformationApp/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace StudentInformationApp.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
